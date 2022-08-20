75dac6fa7870e8b94be2f639e52ea0e2318f3c21
Allochka11 <allavelychko11@gmail.com>
2022-08-20 18:09:28 +0300

    5 lesson function sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 
 function App() {
-  type StudentsType = {
-    name:string
-    age:number
-    isMarried: boolean
-    scores:number
-  }
-  const students = [
-    {name:"Bob", age:26, isMarried: true,scores: 85},
-    {name:"Alex", age:21, isMarried: true,scores: 89},
-    {name:"Nick", age:2, isMarried: true,scores: 120},
-    {name:"John", age:19, isMarried: true,scores: 100},
-  ];
+    // type StudentsType = {
+    //     name: string
+    //     age: number
+    //     isMarried: boolean
+    //     scores: number
+    // }
+    //
+    //
+    // const [students, setStudents] = useState<StudentsType[]>([
+    //     {name: "Bob", age: 26, isMarried: true, scores: 85},
+    //     {name: "Alex", age: 21, isMarried: true, scores: 89},
+    //     {name: "Alla", age: 2, isMarried: false, scores: 120},
+    //     {name: "John", age: 19, isMarried: true, scores: 100},])
+    //
+    // console.log(students)
+    // useEffect(() => {
+    //     setStudents(students.map(el => el.name === 'Alla' ? {...el, isMarried: true} : el))
+    // }, [])
 
 
+    return (
+        <div className={'App'}>
+        </div>
 
-
-  return (
-    <div className="App">
-
-
-
-
-    </div>
-  );
+    );
 }
 
 export default App;
